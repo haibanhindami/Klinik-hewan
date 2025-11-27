09d13019735bc08345456af4de09d7005b56236d
Haiban <haibanhindami@students.unnes.ac.id>
2025-11-27 17:33:16 +0700

    benerin nabrak part 2

--- web_klinik_hewan/web_klinik_hewan/urls.py
+++ web_klinik_hewan/web_klinik_hewan/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import home
 from .views import daftar_customer
-from .models import JadwalPemeriksaan, Pemeriksaan
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('customer/daftar/', daftar_customer, name='daftar_customer'),
     path('fasilitas/', include('fasilitas.urls')),
-    admin.site.register(JadwalPemeriksaan),
-    admin.site.register(Pemeriksaan),
+    path('pemeriksaan/', include('pemeriksaan.urls')),
+    path('pegawai/', include('pegawai.urls')),
+    path('pembayaran/', include('pembayaran.urls')),
+    path('dashboard/', include('dashboard.urls')),
 ]
