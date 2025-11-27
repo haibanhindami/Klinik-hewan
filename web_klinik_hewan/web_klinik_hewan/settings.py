d43bb7da6e8ebc018861bdd4d44d78c8f2fe18e0
marsaavanisf <marsaavanisf24@students.unnes.ac.id>
2025-11-27 17:58:59 +0700

    pemeriksaan

--- web_klinik_hewan/web_klinik_hewan/settings.py
+++ web_klinik_hewan/web_klinik_hewan/settings.py
@@ -37,8 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'customer',
     'pemeriksaan',
-    # 'manajemen_pemeriksaan',
+    'fasilitas'
+    'pegawai',
+    'pembayaran',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
